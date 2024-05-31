50620b52c4f272cad0d4c8e95e470124315ce15f
jenoch <mourgraine12@gmail.com>
2024-05-31 09:59:59 -0400

    Added tearDown() method for database initialization tests

--- tests/database_tests.py
+++ tests/database_tests.py
@@ -1,4 +1,4 @@
-import database.locker_db
+# import database.locker_db
 import sqlite3
 from database import locker_db
 import os
@@ -8,17 +8,17 @@ import unittest
 
 
 class TestDatabaseFuncs(unittest.TestCase):
-
     # Unit Test to test that the database and tables are being created correctly.
+
     def test_create_db(self) -> None:
         # Create a brand new locker DB file using SQLite3
-        test1 = database.locker_db.create_database('test.db')
+        test1 = locker_db.create_database("test.db")
 
         # Attempt to create a duplicate DB with the same name
-        test2 = database.locker_db.create_database('test.db')
+        test2 = locker_db.create_database("test.db")
 
-        self.assertEqual(test1, True, 'Test1 Create DB Fail')
-        self.assertEqual(test2, False, 'Test2 Create DB Fail')
+        self.assertEqual(test1, True, "Test1 Create DB Fail")
+        self.assertEqual(test2, False, "Test2 Create DB Fail")
 
         test_db_conn = sqlite3.connect('test.db')
         test_db_cursor = test_db_conn.cursor()
@@ -46,7 +46,24 @@ class TestDatabaseFuncs(unittest.TestCase):
         self.assertEqual(test4, 1, "Test4 Fail - Items table not present.\n")
         self.assertEqual(test5, 1, "Test5 Fail - Locker Doors table not present.\n")
 
+        # Close the connection before trying to do anything to the file - DOY!!!
+        test_db_conn.close()
         return
+
+    def test_is_sql_db(self) -> None:
+        # This is here in case tearDown() deletes it before it can be accessed
+        locker_db.create_database("test.db")
+
+        test1 = locker_db.isSqlite3Db("test.db")
+        test2 = locker_db.isSqlite3Db("database_tests.py")
+
+        self.assertEqual(test1, True, "Test1 Is_SQL_DB Fail")
+        self.assertEqual(test2, False, "Test2 Is_SQL_DB Fail")
+
+    def tearDown(self) -> None:
+        # Delete the database file for teardown (if it exists)
+        if os.path.exists("test.db"):
+            os.remove("test.db")
 
 
 class TestEmployeeCRUD(unittest.TestCase):
