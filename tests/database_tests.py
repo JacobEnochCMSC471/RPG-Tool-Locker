10341ec57f79cfd3bbdd8e4f00e2e741a67a5ac6
jenoch <mourgraine12@gmail.com>
2024-06-24 12:52:32 -0400

    Added comments to tests to provide brief descriptions of what the test aims to accomplish and what is expected.

--- tests/database_tests.py
+++ tests/database_tests.py
@@ -61,18 +61,23 @@ class TestDatabaseFuncs(unittest.TestCase):
 
 class TestEmployeeCRUD(unittest.TestCase):
     def setUp(self) -> None:
+        # Before each test, create, connect to and retrieve a cursor object from the database. Wipe employee table.
         locker_db.create_database("test.db")
         self.db_conn = sqlite3.connect("test.db")
         self.db_cursor = self.db_conn.cursor()
         self.db_cursor.execute("DELETE FROM employees")
 
     def tearDown(self) -> None:
+        # After each test, close connection and cursor to database and delete the database file if present.
         self.db_cursor.close()
         self.db_conn.close()
         if os.path.exists("test.db"):
             os.remove("test.db")
 
     def test_insert_func_single_correct(self) -> None:
+        # Test a single correct employee item insert. Check return value and presence in employees table.
+        # Should return True and the table should have one item in it.
+
         single_insert_correct = [("12345", "Jake Enoch", 5)]
 
         test8 = locker_db.add_employee(self.db_cursor, single_insert_correct)
@@ -83,6 +88,9 @@ class TestEmployeeCRUD(unittest.TestCase):
         self.assertListEqual([('12345', 'Jake Enoch', 5)], test9, "Test9 Failed: Single Insert Correct")
 
     def test_insert_func_single_incorrect(self) -> None:
+        # Test a single incorrect employee item insert. Check return value and presence in employees table.
+        # Should return False and the table should be empty.
+
         single_insert_incorrect = [(12345, "Jake Enoch", 5)]
 
         locker_db.add_employee(self.db_cursor, [("12345", "Jake Enoch", 5)])  # Insert a correct record first
@@ -95,6 +103,11 @@ class TestEmployeeCRUD(unittest.TestCase):
         self.assertListEqual([('12345', 'Jake Enoch', 5)], test11, "Test11 Failed: Single Insert Incorrect")
 
     def test_insert_func_multiple_correct(self) -> None:
+        # Test inserting multiple correctly formatted employees into the table. Check return value and presence
+        # in table.
+
+        # Should return True and the employees table should have 2 items in it.
+
         multiple_insert_correct = [("123", "John Enoch", 4), ("124", "Nick Enoch", 3)]
 
         locker_db.add_employee(self.db_cursor, [("12345", "Jake Enoch", 5)])  # Insert a correct record first
@@ -108,6 +121,12 @@ class TestEmployeeCRUD(unittest.TestCase):
                              "Test13 Failed: Multi Insert")
 
     def test_multi_insert_multiple_incorrect(self) -> None:
+        # Test inserting multiple incorrectly formatted employees into employee table. Check return val and presence
+        # in table.
+
+        # Should return False and there should be no employees in the table.
+
+
         multiple_insert_incorrect = [("125", "Thomas Saddler", "4"), ("126", "Nick Enoch", 3)]
 
         locker_db.add_employee(self.db_cursor, [("12345", "Jake Enoch", 5), ("123", "John Enoch", 4),
@@ -122,6 +141,9 @@ class TestEmployeeCRUD(unittest.TestCase):
                              "Test15 Failed: Multi Insert Incorrect")
 
     def test_delete_existing_employee_correct(self) -> None:
+        # Test deleting an employee that exists in the table. Add employee first. Check return value + presence in table.
+        # Should return True and there should be 2 employees in the employees table after successful deletion.
+
         locker_db.add_employee(self.db_cursor, [("12345", "Jake Enoch", 5), ("123", "John Enoch", 4),
                                                 ("124", "Nick Enoch", 3)])  # Insert records first
 
@@ -134,6 +156,11 @@ class TestEmployeeCRUD(unittest.TestCase):
                              "Test17 Failed: Remove Existing Employee")
 
     def test_delete_existing_employee_incorrect(self) -> None:
+        # Attempt to delete an item in the table using an incorrectly typed employee ID.
+        # Check return value and presence of employee with id "124" in table.
+
+        # Should return False and the employee with ID "124" should still be in the employees table. 2 records total.
+
         locker_db.add_employee(self.db_cursor,
                                [("12345", "Jake Enoch", 5), ("124", "Nick Enoch", 3)])  # Insert records first
 
@@ -146,6 +173,11 @@ class TestEmployeeCRUD(unittest.TestCase):
                              "Test19 Failed: Remove Existing Employee Wrong Type")
 
     def test_delete_nonexistent_employee(self) -> None:
+        # Attempt to delete an employee that doesn't exist in the employees table. Check return value and
+        # presence in table.
+
+        # Should return False and the number of employees in the table should remain at 2.
+
         locker_db.add_employee(self.db_cursor,
                                [("12345", "Jake Enoch", 5), ("124", "Nick Enoch", 3)])  # Insert records first
 
@@ -158,6 +190,11 @@ class TestEmployeeCRUD(unittest.TestCase):
                              "Test21 Fail: Remove Nonexistent Employee")
 
     def test_update_employee_correct(self) -> None:
+        # Test correctly updating an employee's data. Check return value and verify that changes occurred to record.
+
+        # Should return True and the employee with id "124" should have their name and permission levels changed.
+        # The id should remain the same.
+
         locker_db.add_employee(self.db_cursor,
                                [("12345", "Jake Enoch", 5), ("124", "Nick Enoch", 3)])  # Insert records first
 
@@ -169,6 +206,11 @@ class TestEmployeeCRUD(unittest.TestCase):
         self.assertListEqual([("124", "Dave Enoch", 5)], test23, "Test23 Fail: Update Employee Check")
 
     def test_update_employee_incorrect(self) -> None:
+        # Test attempting to update employee record with incorrectly formatted tuples. Check return val and
+        # update status in table.
+
+        # Should return False for all return val tests and the records should remain unchanged.
+
         locker_db.add_employee(self.db_cursor, [("12345", "Jake Enoch", 5)])  # Insert a correct record first
 
         # (name, perm_level) is correct formatting
@@ -191,6 +233,9 @@ class TestEmployeeCRUD(unittest.TestCase):
         self.assertListEqual([("12345", "Jake Enoch", 5)], test27, "Test27 Fail: Incorrect Update 3 Parameters")
 
     def test_get_employee_exists(self) -> None:
+        # Test to make sure retrieving an employee is correct. Check return value (list).
+        # Should return a tuple containing a single employee.
+
         locker_db.add_employee(self.db_cursor, [("12345", "Jake Enoch", 5)])  # Insert a correct record first
 
         test28 = locker_db.get_employee(cursor=self.db_cursor, emp_id="12345")
@@ -198,6 +243,9 @@ class TestEmployeeCRUD(unittest.TestCase):
         self.assertEqual(('12345', 'Jake Enoch', 5), test28, "Test28 Failed: Get Employee (Exists)")
 
     def test_get_employee_dne(self) -> None:
+        # Test functionality of retrieve function when an employee does not exist. Check return type (list).
+        # Should return None since there is nothing in the table to retrieve.
+
         locker_db.add_employee(self.db_cursor, [("12345", "Jake Enoch", 5)])  # Insert a correct record first
 
         test29 = locker_db.get_employee(cursor=self.db_cursor, emp_id="007")
@@ -205,6 +253,8 @@ class TestEmployeeCRUD(unittest.TestCase):
         self.assertEqual(None, test29, "Test29 Failed: Get Employee (does not exist)")
 
     def test_get_all_employees(self) -> None:
+        # Test functionality of get_all_employees() function.
+        # Should return a list of tuples of size 2.
         locker_db.add_employee(self.db_cursor,
                                [("12345", "Jake Enoch", 5), ("124", "Dave Enoch", 5)])  # Insert correct records first
 
@@ -217,18 +267,23 @@ class TestEmployeeCRUD(unittest.TestCase):
 class TestItemCRUD(unittest.TestCase):
 
     def setUp(self) -> None:
+        # Before every test create a new database, connect to it, retrieve the cursor and remove any existant items from DB.
         locker_db.create_database("test.db")
         self.db_conn = sqlite3.connect("test.db")
         self.db_cursor = self.db_conn.cursor()
         self.db_cursor.execute("DELETE FROM items")
 
     def tearDown(self) -> None:
+        # After each test, close the connection and cursor and delete the database file if present.
         self.db_cursor.close()
         self.db_conn.close()
         if os.path.exists("test.db"):
             os.remove("test.db")
 
     def test_add_single_item_correct(self) -> None:
+        # Test adding a single item correctly to the database.
+        # Check return value and presence in table.
+
         items_to_add = [("TL001", "Flathead Screwdriver", "6in length", 0, "001", 1)]
 
         test31 = locker_db.add_item(cursor=self.db_cursor, list_of_items=items_to_add)
@@ -240,8 +295,9 @@ class TestItemCRUD(unittest.TestCase):
         self.assertListEqual(items_to_add, test32, "Test 32 Failed - Single Insert")
 
     def test_add_single_item_incorrect(self) -> None:
-        # Test to make sure we catch all possible type errors when a new item is being added
 
+        # Test to make sure we catch all possible type errors when a new item is being added.\
+        # For each item, test return value and presence in table.
         items_to_add1 = [(1, "Flathead Screwdriver", "6in length", 0, "001", 1)]
         items_to_add2 = [("TL001", 1, "6in length", 0, "001", 1)]
         items_to_add3 = [("TL001", "Flathead Screwdriver", 1, 0, "001", 1)]
@@ -253,6 +309,7 @@ class TestItemCRUD(unittest.TestCase):
 
         counter = 33
 
+        # Iteratively check all 6 items insetad of using a big non-dynamic block of code to check each one individually.
         for item in items_list:
             curr_test_return = locker_db.add_item(self.db_cursor, list_of_items=item)
             curr_test_retrieve = self.db_cursor.execute("SELECT * FROM items").fetchall()
@@ -268,6 +325,9 @@ class TestItemCRUD(unittest.TestCase):
         print(f"Test Counter: {counter}")
 
     def test_empty_insert(self) -> None:
+        # Test if an empty list, a list with an empty tuple and an empty tuple can be added to the table.
+        # Check return type and presence in table.
+
         counter = 46
 
         # Test whether these empty lists/tuples can be added to the item table
